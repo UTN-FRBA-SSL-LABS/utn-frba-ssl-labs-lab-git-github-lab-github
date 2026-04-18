f8a3d7d5251a26cd0ae6f6553a7048352beec4da
Florencia lopez <florlopez@frba.utn.edu.ar>
2026-04-18 19:12:28 -0300

    agrega comentario explicativo en multiplizar

--- operaciones.c
+++ operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+/* recibe dos numeros y devuelve la multi entre ellos */
 int multiplicar(int a, int b) {
     return a * b;
 }
