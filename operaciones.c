bb1f83e7792adb3705761a3cc14500f79082fc36
Bombakengz <notdrakengzyet@gmail.com>
2026-04-18 17:07:05 -0300

    Mejora: Agrego comentario explicativo en multiplicar

--- operaciones.c
+++ operaciones.c
@@ -14,6 +14,7 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
+/* Multiplica dos enteros retornando su producto */
 int multiplicar(int a, int b) {
     (void)a; (void)b;
     return 0; /* TODO: implementar */
