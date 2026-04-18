6227bc2cfb7d34e93d6057bc903144e0d6e15391
Brian Chejo <bchejohuanca@frba.utn.edu.ar>
2026-04-18 17:17:46 -0300

    Agregado de cometario explicativo en la  funcion multiplicar

--- operaciones.c
+++ operaciones.c
@@ -13,8 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
-// Implementa la función multiplicar que reciba dos enteros y retorne su producto.
+// Implementar la función multiplicar que reciba dos enteros y retorne su producto.
 int multiplicar(int a, int b) {
     return a * b;
 }
