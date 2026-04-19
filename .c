aed80ef72587abdf3409b543c72f8b7449f849a0
Megg-7 <mgonzalesrodriguez@frba.utn.edu.ar>
2026-04-19 14:07:13 -0300

    Resolucion parte 1 : Mi primera branch y mi primer PR

---
+++ .c
@@ -0,0 +1,14 @@
+[1mdiff --git a/operaciones.c b/operaciones.c[m
+[1mindex 2c9a622..d285d5b 100644[m
+[1m--- a/operaciones.c[m
+[1m+++ b/operaciones.c[m
+[36m@@ -15,8 +15,7 @@[m [mint restar(int a, int b) {[m
+ /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */[m
+ [m
+ int multiplicar(int a, int b) {[m
+[31m-    (void)a; (void)b;[m
+[31m-    return 0; /* TODO: implementar */[m
+[32m+[m[32m    return  a * b; /* TODO: implementar */[m
+ }[m
+ [m
+ /* ── esPar ────────────────────────────────────────────────────────────────── */[m
--- operaciones.c
+++ operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+/*Una linea funciona de modo que tengamos una linea principal que tenga el codigo que funciona y que al agregar cambios utilicemos ramas paralelas que una vez que tengan un funcionamiento correcto sean integradas a la linea principal*/
 int multiplicar(int a, int b) {
     return  a * b; /* TODO: implementar */
 }
