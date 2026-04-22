79b65c6b8baa69d874b1a316846aae5e67214357
Florencia Ayelen Gerez <fgerez@frba.utn.edu.ar>
2026-04-22 16:27:57 -0300

    Agrega comentario explicativo en multiplicar

--- operaciones.c
+++ operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+/* Calcula el producto de dos numeros enteros */
 int multiplicar(int a, int b) {
 return a * b;
 }
