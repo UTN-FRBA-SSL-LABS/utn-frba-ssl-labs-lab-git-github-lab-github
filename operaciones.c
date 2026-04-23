7f8a872d7e3924332c689feeacdbb48278b059c9
Sofia <sloisi@frba.utn.edu.ar>
2026-04-23 20:37:19 -0300

    Atiende review: ajusta uso de const

--- operaciones.c
+++ operaciones.c
@@ -14,7 +14,7 @@ int restar(const int a, const int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 // esta funcion multiplica el primer valor x el segundo valor y devuelve el resultado
-int multiplicar(const int a, const int b) {
+int multiplicar(int a, int b) {
     return a * b;
 }
 
