87b3663a63bcc7f6c3552becce68b156d1818221
Ben Moreau <bigben.mro@gmail.com>
2026-04-26 04:07:39 -0300

    Atiende review: Agrego comentario descriptivo

--- operaciones.c
+++ operaciones.c
@@ -14,7 +14,7 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
-//Recibo dos parametros enteros y devuelvo el resultado de multiplicarlos
+//La función multiplicar exige dos parámetros de tipo entero [int] y retorna el resultado de aplicar el producto aritmético a los valores indicados, este retorno es a su vez de tipo entero [int].
 int multiplicar(int a, int b) {
     return a * b;
 }
