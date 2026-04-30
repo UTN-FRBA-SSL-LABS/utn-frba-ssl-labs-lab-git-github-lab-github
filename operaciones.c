45eacc6780159f8d1e09600d3e53f97beaf5cbe8
Martina Pecorelli <mpecorelli@frba.utn.edu.ar>
2026-04-30 18:06:44 -0300

    agrega comentario

--- operaciones.c
+++ operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+//la funcion multiplicar toma dos enteros a y b, y devuelve el resultado de multiplicarlos. La función utiliza el operador de multiplicación (*) para realizar la operación y retorna el resultado.
 int multiplicar(int a, int b) {
     return a * b;
 }
