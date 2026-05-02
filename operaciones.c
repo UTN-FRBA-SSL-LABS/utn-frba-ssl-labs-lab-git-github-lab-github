bcd449b9a4cf246aec7cec22e831bfd912fc7a8e
lucas <luc.bl1105@gmail.com>
2026-05-02 17:17:16 -0300

    Mejora: Cambios realizados a operacion c en base a comentarios y constantes

--- operaciones.c
+++ operaciones.c
@@ -2,18 +2,19 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 
-int sumar(int a, int b) {
+int sumar(int a, const int b) {
     return a + b;
 }
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
-int restar(int a, int b) {
+int restar(int a, const int b) {
     return a - b;
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 // Esta funcion realiza la multiplicacion de dos enteros
+// recibiendo un entero y otro entero se multiplica el primer elemento por el segundo
 int multiplicar(int a, int b) {
     return a * b;
 }
