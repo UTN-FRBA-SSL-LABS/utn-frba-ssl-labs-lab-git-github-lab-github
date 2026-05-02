413e4f6340bab8b49d6d9b3df79e71084e44f590
Melany Torrejon <mtorrejo@frba.utn.edu.ar>
2026-05-02 18:42:03 -0300

    Mejora: se agrego const a los parámetros

--- operaciones.c
+++ operaciones.c
@@ -2,24 +2,24 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 
-int sumar(int a, int b) {
+int sumar(const int a, const int b) {
     return a + b;
 }
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
-int restar(int a, int b) {
+int restar(const int a, const int b) {
     return a - b;
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 /*Una linea funciona de modo que tengamos una linea principal que tenga el codigo que funciona y que al agregar cambios utilicemos ramas paralelas que una vez que tengan un funcionamiento correcto sean integradas a la linea principal*/
-int multiplicar(int a, int b) {
+int multiplicar(const int a, const int b) {
     return  a * b; /* TODO: implementar */
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
-int esPar(int n) {
+int esPar(const int n) {
     return (n % 2) == 0;
 }
