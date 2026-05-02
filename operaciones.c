11c6ff8fca139792836a6e9329ef13c26ea62a0c
Stefano <stefxyz01@gmail.com>
2026-05-02 15:20:14 -0300

    Atiende review: se agrego los const que faltaban

--- operaciones.c
+++ operaciones.c
@@ -9,14 +9,14 @@ int sumar(const int a, const int b)
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
-int restar(int a, int b)
+int restar(const int a, const int b)
 {
     return a - b;
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 /* Esta función recibe dos enteros y devuelve su producto*/
-int multiplicar(int a, int b)
+int multiplicar(const int a, const int b)
 {
     return a * b;
 }
