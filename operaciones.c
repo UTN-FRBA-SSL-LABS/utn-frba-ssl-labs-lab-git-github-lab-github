46ad9fd3ccae0b21d2f4c64003c407bf20993517
Nicolás Basile <nbasile@frba.utn.edu.ar>
2026-05-02 14:45:48 -0300

    Mejora: agrego const a funcion sumar, restar y multiplicar y descripcion de la funcion multiplicar

--- operaciones.c
+++ operaciones.c
@@ -2,21 +2,21 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 
-int sumar(int a, int b)
+int sumar(const int a, const int b)
 {
     return a + b;
 }
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
-int restar(int a, int b)
+int restar(const int a, const int b)
 {
     return a - b;
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
-int multiplicar(int a, int b)
+/* Calcula el producto de dos números enteros y retorna el resultado*/
+int multiplicar(const int a, const int b)
 {
     return a * b;
 }
