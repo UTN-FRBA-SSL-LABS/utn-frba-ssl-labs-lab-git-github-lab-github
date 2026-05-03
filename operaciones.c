c3cc286bcede15ba9cbf3325a16e964ee86de573
julietac64 <julycrig@gmail.com>
2026-05-03 14:25:52 -0300

    Atiende review: Agrego const a los parametros

--- operaciones.c
+++ operaciones.c
@@ -15,7 +15,7 @@ int restar(int a, int b) {
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
 // multiplica dos valores enteros y retorna el valor
-int multiplicar(int a, int b) {
+int multiplicar(const int a, const int b) {
     return a * b;
 }
 
