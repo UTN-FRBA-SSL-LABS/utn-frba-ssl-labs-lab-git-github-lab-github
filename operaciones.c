55bc6fbfec8e9cc28ad38a3375afd8d658e78521
Facundo Cordoba <fcordobanigro@frba.utn.edu.ar>
2026-05-03 17:29:33 -0300

    Atiende review: Comentarios extendidos

--- operaciones.c
+++ operaciones.c
@@ -4,17 +4,17 @@
 
 int sumar(int a, int b) {
     return a + b;
-} //sumar dos valores y devuelve su suma mediante el operador +
+} //sumar dos valores y devuelve su suma mediante el operador +. a partir de un valor a y b devuelve la suma de ambos
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
 int restar(int a, int b) {
     return a - b;
-} //restar dos valores y devuelve su diferencia mediante el operador -
+} //restar dos valores y devuelve su diferencia mediante el operador -. a partir de un valor a y b devuelve la resta de ambos
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
-// muiltiplicar recibe dos valores y devuelve su producto meidante el operador *
+// muiltiplicar recibe dos valores y devuelve su producto meidante el operador *. a partir de un valor a y b devuelve la multiplicacion de ambos
 int multiplicar(int a, int b) {
     return a * b;
 }
