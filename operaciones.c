922e5f393442d1b91564835a01ef60a7c3ab8752
Facundo Cordoba <fcordobanigro@frba.utn.edu.ar>
2026-05-03 17:14:19 -0300

    Mejora: Agregados comentarios a las funciones de suma y resta

--- operaciones.c
+++ operaciones.c
@@ -4,17 +4,17 @@
 
 int sumar(int a, int b) {
     return a + b;
-}
+} //sumar dos valores y devuelve su suma mediante el operador +
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
 int restar(int a, int b) {
     return a - b;
-}
+} //restar dos valores y devuelve su diferencia mediante el operador -
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
-// muiltiplicar recibe dos valores y devuelve su producto meidante el operador *
+// multiplicar recibe dos valores y devuelve su producto mediante el operador *
 int multiplicar(int a, int b) {
     return a * b;
 }
