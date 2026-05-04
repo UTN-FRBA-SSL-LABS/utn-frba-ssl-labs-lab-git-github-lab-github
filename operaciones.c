4b5f0ef68ad0d54b1fbf14443c82453ea876914e
RosenbergTeo <rosenbergteo@gmail.com>
2026-05-04 17:35:13 -0300

    Mejora en el comentario de funcion Multiplicar

--- operaciones.c
+++ operaciones.c
@@ -12,8 +12,8 @@ int restar(int a, int b) {
     return a - b;
 }
 
-/* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-/*  Multiplica dos números enteros y devuelve el resultado */
+/* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── *
+/*  Multiplica dos números enteros de tipo "int" usando el operador * y devuelve el resultado */
 int multiplicar(int a, int b) {
     return a*b; /* TODO: implementar */
 }
