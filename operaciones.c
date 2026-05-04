d17a66aa00e79de7df7d7349c44d5c194c4e8366
Uma <udemugica@frba.utn.edu.ar>
2026-05-04 10:41:23 -0300

    Corrige multiplicar

--- operaciones.c
+++ operaciones.c
@@ -14,19 +14,15 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
-<<<<<<< HEAD
 // Multiplica dos numeros enteros y devuelve el resultado
 int multiplicar(int a, int b) {
     return a * b;
-=======
-int multiplicar(int a, int b) {
-    (void)a; (void)b;
-    return 0; /* TODO: implementar */
->>>>>>> origin/feature/conflicto-demo
 }
+
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
 int esPar(int n) {
     return (n % 2) == 0; /* version main*/
 }
+
