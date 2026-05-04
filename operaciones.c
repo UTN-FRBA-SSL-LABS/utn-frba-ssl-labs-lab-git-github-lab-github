f27f828c05a13b4a3573d8d5848c2e2d165c14c3
ramazef <ramrodriguez@frba.utn.edu.ar>
2026-05-04 16:38:06 -0300

    Mejora: comentario de funcion multiplicar

--- operaciones.c
+++ operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-//Acá multiplicamos a * b
+//Acá multiplicamos a * b, ambos son enteros
 int multiplicar(int a, int b) {
     return a * b;
 }
