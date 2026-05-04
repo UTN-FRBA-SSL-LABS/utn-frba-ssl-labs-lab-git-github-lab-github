25882d5fb51a4a8dfadba5c7dfae8ecbb7e12aae
davidf088 <dfuertesmamani@frba.utn.edu.ar>
2026-05-04 18:05:59 -0300

    Mejora: resolucion de la multiplicacion y comentario mas descriptivo de multiplicar()

--- operaciones.c
+++ operaciones.c
@@ -13,10 +13,9 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+//la función multiplicar toma una variable tipo a y b, luego los multiplica
 int multiplicar(int a, int b) {
-    (void)a; (void)b;
-    return 0; /* TODO: implementar */
+    return a * b;
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
