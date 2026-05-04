1eae45c40d2328aed24ac8036cef869102cee83a
Elmer Flores <eflores@frba.utn.edu.ar>
2026-05-04 17:51:53 -0300

    Revert "wip: experimento roto"
    This reverts commit a19336a7ffa827490ea95b89c77e869ca52e0298.

--- operaciones.c
+++ operaciones.c
@@ -23,9 +23,3 @@ int multiplicar(int a, int b) {
 int esPar(int n) {
     return (n % 2) == 0;
 }
-
-/* ── dividir  ─────────────────────────────────────────────────────────────── */
-int dividir(int a, int b) {
-    return a - b; /* bug intencional */
-}
-
