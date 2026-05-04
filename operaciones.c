b205d4afc5d93db1302cb33f1d97fe5159ea763c
GabrielCenturio <gcenturiocayo@frba.utn.edu.ar>
2026-05-04 17:35:23 -0300

    Atiende review: agrgue un comentario arriba de la funcion sumar

--- operaciones.c
+++ operaciones.c
@@ -1,7 +1,7 @@
 #include "operaciones.h"
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
-
+//se respondio el PR
 int sumar(const int a,const int b) {
     return a + b;
 }
