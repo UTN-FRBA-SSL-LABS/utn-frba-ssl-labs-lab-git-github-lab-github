6fe79ff4b3ddfe0e94e0bbf6cc34055f30738c29
Sergio Daniel Vargas <sevargas@frba.utn.edu.ar>
2026-05-04 23:40:11 -0300

    Ajusta esPar en main con comentario de version

--- operaciones.c
+++ operaciones.c
@@ -21,10 +21,7 @@ int multiplicar(const int a, const int b) {
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
 int esPar(int n) {
-    return (n % 2) == 0;
+    return (n % 2) == 0; /* version main */
 }
 
 
-int dividir (int a, int b){
-    return a - b; /* bug intencional*/
-}
